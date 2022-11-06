913895048fd5dbf630db9dd4b5c65085179b5a98
Aniket Bhosale <30499657+aniketbhosale2808@users.noreply.github.com>
2022-11-07 02:10:15 +0530

    Modifies data extractor so that all datasets can be extracted from the source in running extractor only once

--- data_extractor/src/exec.py
+++ data_extractor/src/exec.py
@@ -2,11 +2,18 @@ from crawler import Crawler
 from scrapper import Scrapper
 from downloader import ZipDownloader
 
-crawl_url = input("Input source url:\t")
-print("Calling Crawler")
-crawler = Crawler(crawl_url)
-print("Calling scrapper")
-scrapper = Scrapper(crawler.url, crawler.response)
-print("Calling downloader")
-downloader = ZipDownloader(scrapper.get_download_link())
-downloader.zip_downloader()
+
+crawl_urls = ["https://genderdata.worldbank.org/topics/employment-and-time-use/",
+"https://genderdata.worldbank.org/topics/education/",
+"https://genderdata.worldbank.org/topics/youth-15-24/"]
+
+for crawl_url in crawl_urls:
+    print("crawl url: ", crawl_url)
+    print("Calling Crawler")
+    crawler = Crawler(crawl_url)
+    print("Calling scrapper")
+    scrapper = Scrapper(crawler.url, crawler.response)
+    print("Calling downloader")
+    downloader = ZipDownloader(scrapper.get_download_link())
+    downloader.zip_downloader()
+    print("downloaded dataset:",crawl_url.split("/")[-2])
