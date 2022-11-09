aa7d8b4db2b8b4a11c9690598345651e5564e22b
Aniket Bhosale <30499657+aniketbhosale2808@users.noreply.github.com>
2022-11-10 02:27:22 +0530

    Removed unnecessary statements from insert_datasets.py

--- databaseconnections/mongodb_connections/src/insert_datasets.py
+++ databaseconnections/mongodb_connections/src/insert_datasets.py
@@ -22,41 +22,29 @@ def insert_data(fname,collection_name):
     mongo_collection = mongo_db[collection_name]
     data = pd.read_csv(fname)
     payload = json.loads(data.to_json(orient='records'))
-    # mongo_collection.remove()
     mongo_collection.insert_many(payload)
     print("Record inserted")
 
+
 path = "./../../../data_extractor/data/*/*.csv"
-
-
 for fname in glob.glob(path):
-    # print(fname)
     splitted = fname.split("\\")
     file_name = splitted[-1].split(".")[0]
     directory_name = splitted[-2]
-    # print("file_name:",file_name)
-    # print("directory:",directory_name)
     mdirectory_name = replace_digits(directory_name)
     modified_file_name = (file_name.lower().replace(" ","_")).replace("-","_")
     mfile_name = replace_digits(modified_file_name)
     if directory_name.lower() == modified_file_name:
         collection_name = mdirectory_name.lower()
         collection_name = re.sub(r"\__{1,}","",collection_name)
-        # print("file_name:",file_name)
-        # print("directory:",directory_name)
-        # print("Collection name:",collection_name)
         insert_data(fname,collection_name)
         print("*==*"*20)
     else: 
-        # print("file_name:",file_name)
-        # print("directory:",directory_name)
         collection_name = mdirectory_name + "_" + modified_file_name.replace("___","_")
         collection_name = re.sub(r"\__{1,}","_",collection_name)
         # re.sub(regex_search_term, regex_replacement, text_before)
-        # print("Collection name:",collection_name)
         insert_data(fname,collection_name)
         print("*==*"*20)
-        
-     
-        #payload = json.loads(df.to_json(orient='records'))
+
+
         
